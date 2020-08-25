3055a551b7628198623e78fd5453b52290dd5274
6ydz642 m <m6ydz642@hotmail.com>
2020-08-25 22:13:54 +0900

    0326 자바

---
+++ Tranning_Java0326/src/Hello.java
@@ -0,0 +1,41 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println(" 안녕하세요 ");
+		System.out.println("실행 단축키 ctrl F11");
+		System.out.println("컴파일 단축키 F11");
+		System.out.println("sysout + ctrl +space");
+		System.out.println();
+		// asdasd;
+		// ctrl + shift + f => 소스코드 정렬
+		// 코드 삭제 (1줄) ctrl + d
+		// ctrl + alr + 방향키 아래 => 코드라인 복사
+
+		// 자바코드
+		// 1. 실행문
+		// 2. 비실행문 => 주석(문)
+		// => 코드가 실행이 안됨(출력X)
+		// => 소스코드의 설명, 컴파일시 테스트목적
+
+		// 주석문
+		// 1) // 한줄짜리 주석 ctrl + shift + c
+		// 2) /* 내용 */ 여러줄 처리 주석 ctrl + shift + / , \
+		/*
+		 * ㅇ ㅇ
+		 */
+		// 3) /** 내용 */ JavaDoc 주석
+		/**
+		 * ㅇㅇㅇ
+		 */
+
+		System.out.println("1");
+		// System.out.println("2");
+		// System.out.println("3");
+		// System.out.println("4");
+		System.out.println("5");
+
+	}
+
+}
